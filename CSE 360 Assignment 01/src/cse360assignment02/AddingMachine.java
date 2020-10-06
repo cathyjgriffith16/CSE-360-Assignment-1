8ca5da186bbb15defe944b6ec3cbf8b58a561b5c
Catherine Griffith <cjgriff5@asu.edu>
2020-10-05 22:52:06 -0700

    Initial version of AddingMachine

---
+++ CSE 360 Assignment 01/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+/* Catherine Griffith
+ * Assignment 1 - Version Control
+ * Dr. Javier Gonzalez Sanchez
+ * Monday, October 5th, 2020
+ * This assignment gives us practice using version controls like Github to post our programs. This is the initial version.
+ * */
+package cse360assignment02;
+
+/*A public class with the purpose of adding numbers **/
+public class AddingMachine {
+  private int total;    
+
+  /*A default constructor that instantiates the object of the AddingMachine class **/
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+
+  /*A getter method for returning all addition operations
+   * Returns sum of the total **/
+  public int getTotal () {
+    return 0;
+  }
+
+  /*This method adds the value to the total
+   *The parameter called value is the amount added to the total **/
+  public void add (int value) {
+  }
+
+  /*This method subtracts the value to the total
+   *The parameter called value is the amount subtracted from the total **/
+  public void subtract (int value) {
+  }
+
+   /*The output should return as a String **/
+  public String toString () {
+    return "";
+  }
+
+   /*Clears memory and resets to the original values**/
+  public void clear() {
+  }
+} 
